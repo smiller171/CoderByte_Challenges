e30c3602a28ac5198bafc022a3bd7b4b234f65fc
Scott Miller <scott.miller171@gmail.com>
2014-02-04 13:29:04 -0500

    sanity check

--- LetterChanges.py
+++ LetterChanges.py
@@ -1,1 +1,14 @@
-def scott.
+def LetterChanges(str):
+	length=len(s)
+	str=str.lower()
+	
+def ChangeLetter(char)
+	if char.isalpha():
+		char=chr(ord(char)+1)
+		char = VowelCheck(char)
+	else:
+		
+
+def VowelCheck(vchk):
+	
+print LetterChanges(raw_input())
