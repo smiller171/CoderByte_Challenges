451328a604d544f5fcef5500f2c55620c96aad8b
Scott Miller <scott.miller171@gmail.com>
2014-02-04 13:58:02 -0500

    bugfix

--- LetterChanges.py
+++ LetterChanges.py
@@ -1,14 +1,28 @@
 def LetterChanges(str):
-	length=len(s)
+	length=len(str)
 	str=str.lower()
+	i=0
+	while i < length:
+		str[i]=ChangeLetter(str[i])
+		i=i+1
+	return str
 	
-def ChangeLetter(char)
+def ChangeLetter(char):
 	if char.isalpha():
 		char=chr(ord(char)+1)
-		char = VowelCheck(char)
+		char=VowelChange(char)
+		return char
 	else:
-		
+		return char
 
-def VowelCheck(vchk):
-	
+def VowelChange(char):
+	if isVowel(char):
+		char=char.upper()
+		return char
+	else:
+		return char
+
+def isVowel(char):
+    return char.lower() in 'aeiou'
+
 print LetterChanges(raw_input())
